7c5e6b9909005519cc06c19f1c43e0be9568bc51
FrederikDreyer <82033515+FrederikDreyer@users.noreply.github.com>
2021-09-28 11:32:36 +0200

    responsiv

--- forside.js
+++ forside.js
@@ -8,3 +8,12 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function myFunction2() {
+  var x = document.getElementById("united");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
